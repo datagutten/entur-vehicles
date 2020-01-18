13c2392f6f2f28b3f3bf624ed99ff90276f5dff3
datagutten <datagutten@datagutten.net>
2020-01-18 13:41:14 +0100

    Add basic setup on travis

---
+++ vehicle_info/vehicle_info/local_settings_test.py
@@ -0,0 +1,16 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'asdf'
