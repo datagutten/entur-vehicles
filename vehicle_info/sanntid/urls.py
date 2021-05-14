bc8978e88653175004cb1279fa90fb7d0f02c2d4
datagutten <datagutten@datagutten.net>
2021-05-14 21:44:33 +0200

    Rename URL patterns

--- vehicle_info/sanntid/urls.py
+++ vehicle_info/sanntid/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('stops_latlon', views.stops_latlon, name='stops_latlon'),
     path('stop/<str:stop>', views.stop_departures, name='stop_departures'),
     path('autocomplete', views.autocomplete, name='autocomplete'),
-    path('vehicles/<str:line>/<str:line2>', views.vehicle_status, name='vehicles2'),
-    path('vehicles/<str:line>', views.vehicle_status, name='vehicles'),
+    path('vehicles/<str:line>/<str:line2>', views.vehicle_status, name='active-vehicles-2line'),
+    path('vehicles/<str:line>', views.vehicle_status, name='active-vehicles'),
     path('departures/quay/<str:quay>.json', views.departures_json, name='departures_quay_json'),
     path('departures/stop/<str:stop>.json', views.departures_json, name='departures_stop_json'),
 ]
