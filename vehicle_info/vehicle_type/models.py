203b99a41db58f65a51bb6a0a4c9e53de16a18ee
datagutten <datagutten@datagutten.net>
2021-05-14 21:27:32 +0200

    Allow fields to be null

--- vehicle_info/vehicle_type/models.py
+++ vehicle_info/vehicle_type/models.py
@@ -14,19 +14,19 @@ class Operator(models.Model):
 class Vehicle(models.Model):
     numlow = models.IntegerField('Internnummer fra')
     numhigh = models.IntegerField('Internnummer til')
-    type = models.CharField('Type/modell', max_length=200, blank=True, default='')
-    length = models.IntegerField('Lengde', blank=True, default=0)
-    year = models.IntegerField('Årsmodell', blank=True, default=0)
-    fuel = models.CharField('Drivstoff', max_length=200, blank=True, default='')
+    type = models.CharField('Type/modell', max_length=200, null=True, blank=True, default='')
+    length = models.IntegerField('Lengde', null=True, blank=True, default=0)
+    year = models.IntegerField('Årsmodell', null=True, blank=True, default=0)
+    fuel = models.CharField('Drivstoff', max_length=200, null=True, blank=True, default='')
     num_prefix = models.IntegerField('Ekstra sifer foran internnummer', blank=True, null=True)
-    operator = models.ForeignKey(Operator, on_delete=models.PROTECT, verbose_name='Operatør')
+    operator = models.ForeignKey(Operator, on_delete=models.PROTECT, verbose_name='Operatør', blank=True, null=True)
 
     def __str__(self):
-        if self.length > 0:
+        if self.length and self.length > 0:
             length = str(self.length) + 'm '
         else:
             length = ''
-        if self.year > 0:
+        if self.year and self.year > 0:
             year = ' ' + str(self.year)
         else:
             year = ''
