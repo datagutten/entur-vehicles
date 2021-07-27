e6ee4c2e902ce083d243fc8d3a6d1bd6d71fda14
datagutten <datagutten@datagutten.net>
2021-07-27 22:01:57 +0200

    Cache quays

--- vehicle_info/sanntid/templatetags/rutedata_tags.py
+++ vehicle_info/sanntid/templatetags/rutedata_tags.py
@@ -2,7 +2,7 @@ from django import template
 from rutedata.models import Line, Quay
 
 register = template.Library()
-
+quay_cache = {}
 
 def line_name(line_ref):
     try:
@@ -16,9 +16,13 @@ register.filter('line_name', line_name)
 
 
 def quay_name(quay_ref):
+    if quay_ref in quay_cache:
+        return quay_cache[quay_ref].Stop.Name
+
     try:
-        quay = Quay.objects.get(id=quay_ref)
-        if not quay.Description:
+        quay = Quay.objects.select_related('Stop').get(id=quay_ref)
+        quay_cache[quay_ref] = quay
+        if not quay.Description or quay.Description == quay_ref:
             return quay.Stop.Name
         else:
             return '%s (%s)' % (quay.Stop.Name, quay.Description)
