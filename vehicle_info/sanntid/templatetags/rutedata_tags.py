e17372ff76b11ab94c01c89c5c7149d2a7847f2f
datagutten <datagutten@datagutten.net>
2021-07-30 10:55:52 +0200

    Rename tag "quay_name" to "quay_stop_name" and add new tag to get the description of the quay itself

--- vehicle_info/sanntid/templatetags/rutedata_tags.py
+++ vehicle_info/sanntid/templatetags/rutedata_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from rutedata.models import Line, Quay
+from typing import Optional
 
 register = template.Library()
 quay_cache = {}
@@ -15,19 +16,48 @@ def line_name(line_ref):
 register.filter('line_name', line_name)
 
 
-def quay_name(quay_ref):
+def quay(quay_ref) -> Optional[Quay]:
+    if quay_ref in quay_cache:
+        return quay_cache[quay_ref]
+    try:
+        quay_obj = Quay.objects.select_related('Stop').get(id=quay_ref)
+        quay_cache[quay_ref] = quay_obj
+        return quay_obj
+    except Quay.DoesNotExist:
+        return None
+
+
+def quay_stop_name(quay_ref):
+    quay_obj = quay(quay_ref)
+    if not quay_obj or not quay_obj.Stop:
+        return quay_ref
+
     if quay_ref in quay_cache:
         return quay_cache[quay_ref].Stop.Name
 
     try:
-        quay = Quay.objects.select_related('Stop').get(id=quay_ref)
-        quay_cache[quay_ref] = quay
-        if not quay.Description or quay.Description == quay_ref:
-            return quay.Stop.Name
+        if not quay_obj.Description or quay_obj.Description == quay_ref:
+            return quay_obj.Stop.Name
         else:
-            return '%s (%s)' % (quay.Stop.Name, quay.Description)
+            return '%s (%s)' % (quay_obj.Stop.Name, quay_obj.Description)
     except Quay.DoesNotExist:
         return quay_ref
 
 
+def quay_name(quay_ref):
+    quay_obj = quay(quay_ref)
+    if not quay_obj:
+        return quay_ref
+    if quay_obj.Description:
+        description = quay_obj.Description
+    elif quay_obj.Stop.Description:
+        description = quay_obj.Stop.Description
+    else:
+        description = ''
+
+    return ('%s %s' % (
+        quay_obj.PublicCode or '', description)).strip()
+
+
 register.filter('quay_name', quay_name)
+register.filter('quay_stop_name', quay_stop_name)
